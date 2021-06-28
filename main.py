01f8e0716254a911190dd8bd747bb3542ea71a06
Jakub Vlcek <jakubvlcek.eu@gmail.com>
2021-06-28 19:55:34 +0200

    flask support for uwsgi

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import dash_bootstrap_components as dbc
 from layout_app1 import serve_layout_app1
 import callbacks
 
-server = Flask(__name__)
-app = Dash(
+app = Flask(__name__)
+dash_app = Dash(
     __name__,
-    server=server,
+    server=app,
+    url_base_pathname="/",
     external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.title = 'Silver Dash'
-app.layout = serve_layout_app1
+dash_app.title = 'Silver Dash'
+dash_app.layout = serve_layout_app1
 
 if __name__ == '__main__':
     app.run_server(host = "0.0.0.0", port = 8090, dev_tools_ui=True, debug=True,
